400a1ae0675b2a30d03aecde4ca63a3e58ab2d51
mtop <mtop.sib@gmail.com>
2020-11-09 17:45:22 +0700

    БД + логирование + мелочи

--- aio_bot.py
+++ aio_bot.py
@@ -4,6 +4,9 @@ https://surik00.gitbooks.io/aiogram-lessons/content/chapter1.html
 from aiogram import Bot, types
 from aiogram.dispatcher import Dispatcher
 from aiogram.utils import executor
+from loguru import logger
+logger.add("log\\file_{time}.log")
+
 
 from config import TOKEN
 
@@ -11,23 +14,31 @@ from config import TOKEN
 bot = Bot(token=TOKEN)
 dp = Dispatcher(bot)
 
-'''
-@dp.message_handler(commands=['start'])
-async def process_start_command(message: types.Message):
-    await message.reply("Привет!\nНапиши мне что-нибудь!")
+def add_mess_db(mess):
+    print('начинаем добавлять мессагу в дата бэйз')
+    print(mess)
+    logger.debug(mess)
+    mess.photo[0].download('test1.jpg')
 
 
-@dp.message_handler(commands=['help'])
-async def process_help_command(message: types.Message):
-    await message.reply("Напиши мне что-нибудь, и я отпрпавлю этот текст тебе в ответ!")
+@dp.message_handler(content_types=['photo'])
+async def handle_docs_photo(message):
+    print('фото детект: ', message )
+    await add_mess_db(message)
+    print('----------------------')
+    #i=message.photo
+    #print(str(i.len()))
+    await message.photo[0].download('test.jpg')
 
-'''
+
+
+
 @dp.message_handler()
 async def echo_message(msg: types.Message):
     print('получено' ,msg.from_user.id, msg.text, msg.chat, msg.from_user, msg.reply_to_message, msg)
     print('msg: ', msg)
     print('----------------------')
-
+    logger.debug(msg)
     #await bot.answer_inline_query()
     await bot.send_message(msg.from_user.id, 'Я возвращаю вам ответ: '+ msg.text)
 
